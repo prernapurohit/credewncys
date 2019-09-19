ed62beba5088050e8be15f08a83d8d0e6ff1df50
preranapurohit <prerana.purohit@credencys.com>
2019-09-19 16:27:58 +0530

    commit

---
+++ paragraph.py
@@ -0,0 +1,1 @@
+print("hi this is prerna.I am working in credencys.\n I have completed my degree in odisha. I have completed my masterdegree in college of it and management eduvcation");
---
+++ string1.py
@@ -0,0 +1,10 @@
+print("hello we are here plz come wait a min\n" + "why we need to wait")
+
+string = "hello we are here plz come\n  wait a min" + "why we need to wait"
+
+
+splitstr=string.split()
+print(splitstr)
+print(splitstr[4] + ' '+splitstr[5]+' '+splitstr[6])
+
+
