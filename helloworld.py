3648c487962d00f6a2fa44c29a26e5e4cd2aca93
preranapurohit <prerana.purohit@credencys.com>
2019-09-19 16:24:15 +0530

    commit

---
+++ helloworld.py
@@ -0,0 +1,74 @@
+"""counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print counter
+print miles
+print name
+
+
+a=b=c=1
+
+print a
+print b
+print c
+
+
+a,b,c=1,2,"prerna"
+
+print a
+print b
+print c
+
+print("hello world")
+
+x=10
+y=20
+
+print x,y
+
+print 7/5
+print -7/5
+print("hello world")
+print "hello world"
+
+def main(fname):
+    print("hello" + fname)
+
+
+main("prerna")
+def state(country="india"):
+    print("this is my" + country)
+
+state("brajil")
+state("odisha")
+state("gujurat")
+
+
+x=1
+if x==1:
+    print("hello")"""
+
+def car_rental_cost(days):
+    cost=35*days
+    if days>=8:
+     
+	cost-=70
+    elif days>=3:
+	cost-=20
+    return cost
+
+
+print(car_rental_cost(5))
+
+x="prerna"
+
+def myfunc():
+	x="purohit"
+	print("my name is " + x);
+
+myfunc()
+print("my surname is" + x)
+
+
+
